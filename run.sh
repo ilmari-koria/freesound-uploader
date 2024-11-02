35132d216be62d7c20b7a4ac454ab0af5ac4139c
ilmari-koria <ilmarikoria@posteo.net>
2024-11-02 18:20:30 +0000

    add http service

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 BASEX="./basex/bin/basex"
+BASEX_HTTP_START="./basex/bin/basexhttp"
+BASEX_HTTP_STOP="./basex/bin/basexhttpstop"
 XQUERY="./upload-to-freesound.xq"
+$BASEX_HTTP_START
 echo "running"
 $BASEX $XQUERY
 echo "done"
+$BASEX_HTTP_STOP
